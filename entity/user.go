96f797d2a42447c04a3f193023cd15d4eb4a15fa
Kawin Viriyaprasopsook <kawin.v@extend-it-resource.com>
2024-02-18 12:53:23 +0700

    🚧 model

---
+++ entity/user.go
@@ -0,0 +1,57 @@
+package entity
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type UserAccount struct {
+	ID          string         `gorm:"size:26;primary_key"`
+	Username    string         `gorm:"size:50;uniqueIndex:udx_username"`
+	Password    string         `gorm:"size:100"`
+	CreatedAt   time.Time      `gorm:"autoCreateTime"`
+	UpdatedAt   time.Time      `gorm:"autoUpdateTime"`
+	DeletedAt   gorm.DeletedAt `gorm:"uniqueIndex:udx_username"`
+	UserProfile UserProfile    `gorm:"foreignKey:UserID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
+	UserAddress []UserAddress  `gorm:"foreignKey:UserID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
+}
+
+func (UserAccount) TableName() string {
+	return "tb_user_account"
+}
+
+type UserProfile struct {
+	ID        string         `gorm:"size:26;primary_key"`
+	UserID    string         `gorm:"size:26;index"`
+	FirstName string         `gorm:"size:100"`
+	LastName  string         `gorm:"size:100"`
+	Email     string         `gorm:"size:100;uniqueIndex:udx_email"`
+	Phone     string         `gorm:"size:20"`
+	Avatar    string         `gorm:"size:255"`
+	CreatedAt time.Time      `gorm:"autoCreateTime"`
+	UpdatedAt time.Time      `gorm:"autoUpdateTime"`
+	DeletedAt gorm.DeletedAt `gorm:"uniqueIndex:udx_email"`
+}
+
+func (UserProfile) TableName() string {
+	return "tb_user_profile"
+}
+
+type UserAddress struct {
+	ID        string         `gorm:"size:26;primary_key"`
+	UserId    string         `gorm:"size:26;index"`
+	Number    string         `gorm:"size:50"`
+	Street    string         `gorm:"size:50"`
+	City      string         `gorm:"size:50"`
+	Province  string         `gorm:"size:50"`
+	Country   string         `gorm:"size:30"`
+	PostCode  string         `gorm:"size:16"`
+	CreatedAt time.Time      `gorm:"autoCreateTime"`
+	UpdatedAt time.Time      `gorm:"autoUpdateTime"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
+
+func (UserAddress) TableName() string {
+	return "tb_user_address"
+}
---
+++ model/user.go
@@ -0,0 +1,26 @@
+package model
+
+type UserAccount struct {
+	ID          string        `json:"id"`
+	Username    string        `json:"username"`
+	Password    string        `json:"password,omitempty"`
+	UserProfile UserProfile   `json:"userProfile"`
+	UserAddress []UserAddress `json:"userAddress"`
+}
+
+type UserProfile struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+	Phone     string `json:"phone"`
+	Avatar    string `json:"avatar"`
+}
+
+type UserAddress struct {
+	Number   string `json:"number"`
+	Street   string `json:"street"`
+	City     string `json:"city"`
+	Province string `json:"province"`
+	Country  string `json:"country"`
+	PostCode string `json:"postCode"`
+}
